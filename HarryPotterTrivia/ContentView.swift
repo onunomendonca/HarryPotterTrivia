a836b510b6912968c3a18c4550493ae17db32e22
Nuno Mendonça <nunomendonca95@gmail.com>
2024-05-24 02:16:07 +0100

    Pilot

---
+++ HarryPotterTrivia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HarryPotterTrivia
+//
+//  Created by Nuno Mendonça on 24/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HarryPotterTrivia/HarryPotterTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterTriviaApp.swift
+//  HarryPotterTrivia
+//
+//  Created by Nuno Mendonça on 24/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct HarryPotterTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
